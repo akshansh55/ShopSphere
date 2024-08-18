0d17823c4a64beb45fd332b3227c3560792dbc7a
akshansh55 <67520085+akshansh55@users.noreply.github.com>
2024-08-19 00:11:52 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,38 +6,50 @@ import CartPage from './pages/CartPage';
 import ProductPage from './pages/ProductPage';
 import NotFoundPage from './pages/NotFoundPage';
 import NavigationBar from './components/Navigation';
+import { CartContext } from './Context/CartContext';
+
 
 
 function App() {
   
-  const [cart, setCart] = useState(JSON.parse(localStorage.getItem('products')));
+  const [cart, setCart] = useState([]);
  
-  function handleAddToCart(productObj){
-   
-        const updatedCart = [...cart, productObj];
-        setCart(updatedCart);
-        localStorage.setItem('products',JSON.stringify(updatedCart))
-        
+  console.log(cart);
   
+  function handleAddToCart(productObj){
+        const updatedCart = [...cart, productObj]
+        setCart(updatedCart);
+        localStorage.setItem('products',JSON.stringify(updatedCart));
   }
 
   function handleRemoveFromCart(productObj){
     let filteredProducts = cart.filter((product)=>{
       return product.id !== productObj.id
     });
-    setCart(filteredProducts);
-    localStorage.setItem('products',JSON.stringify(filteredProducts))
-   
+    setCart(filteredProducts); 
+    localStorage.setItem('products', JSON.stringify(filteredProducts));
   }
+  function increaseQuantity(productObj,quantity,setQuantity){
+    
+    setQuantity((quantity)=>quantity+1);
+  }
+  function decreaseQuantity(productObj,quantity,setQuantity){
+    if(quantity===-1) return;
+    setQuantity((quantity)=>quantity-1);
+  }
+
   return (
     <div className="App">
         <BrowserRouter>
-          <NavigationBar/>
+         <CartContext.Provider value={{cart, handleAddToCart , handleRemoveFromCart, setCart, increaseQuantity,decreaseQuantity}}> 
+         <NavigationBar/>
           <Switch>
               <Route path="/" component={ProductPage} exact={true}/>
               <Route path="/cart" component={CartPage} exact={true}/>
               <Route  component={NotFoundPage}/>
-          </Switch>          
+          </Switch>  
+         </CartContext.Provider>
+                  
         </BrowserRouter>
     </div>
   );
